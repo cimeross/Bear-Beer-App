7f04a81fe3f84d9c6d7ff96b2312e86e5d012e57
Milan Suvajac <milansuvajac021@gmail.com>
2023-04-13 23:20:40 +0200

    feat: Added Create Item component where user can add new item

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { StateProvider } from "./context/StateProvider";
-import { initialstate } from "./context/initialState";
+import { initialState } from "./context/initialState";
 import reducer from "./context/reducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Router>
-			<StateProvider initialState={initialstate} reducer={reducer}>
-				<App />
-			</StateProvider>
-		</Router>
-	</React.StrictMode>
+	<Router>
+		<StateProvider initialState={initialState} reducer={reducer}>
+			<App />
+		</StateProvider>
+	</Router>
 );
