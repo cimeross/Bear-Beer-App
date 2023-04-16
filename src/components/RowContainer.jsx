65a2fb9276b9b72b220b216cf1de6cc9a60e572d
Milan Suvajac <milansuvajac021@gmail.com>
2023-04-16 21:21:08 +0200

    feat: Created cart component and added styling and functionality

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { RiShoppingBasketLine } from "react-icons/ri";
 import { motion } from "framer-motion";
 import NotFound from "../assets/img/NotFound.svg";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const RowContainer = ({ flag, data, scrollValue }) => {
 	const rowContainer = useRef();
+
+	const [items, setItems] = useState([]);
+
+	const [{ cartItems }, dispatch] = useStateValue();
+
+	const addToCart = (item) => {
+		const alreadyInCart = cartItems.some((cartItem) => cartItem.id === item.id);
+		if (!alreadyInCart) {
+			setItems([...items, item]);
+			dispatch({
+				type: actionType.SET_CARTITEMS,
+				cartItems: [...cartItems, item],
+			});
+			localStorage.setItem("cartItems", JSON.stringify([...cartItems, item]));
+		}
+	};
 
 	useEffect(() => {
 		rowContainer.current.scrollLeft += scrollValue;
@@ -39,7 +57,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
 							<motion.div
 								whileTap={{ scale: 0.75 }}
 								className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
-								onClick={() => setItems([...cartItems, item])}
+								onClick={() => addToCart(item)}
 							>
 								<RiShoppingBasketLine className="text-white" />
 							</motion.div>
